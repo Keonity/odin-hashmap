af8f8a7b9032892cd576153c3aa70db3e38783a2
Keonity <56704285+Keonity@users.noreply.github.com>
2024-09-23 21:46:36 -0700

    Finished assignment per Odin Project spec.

--- src/hashmap.js
+++ src/hashmap.js
@@ -4,26 +4,27 @@ class HashMap {
     constructor(size = 16) {
         this.hashmap = new Array(size);
         this.capacity = size;
+        this.loadFactor = 0.75;
         this.size = 0;
     }
 
     hash(key) {
         let hashcode = 0;
-
+        
         const primeNumber = 73;
         for (let i = 0; i < key.length; i++) {
             hashcode = primeNumber * hashcode + key.charCodeAt(i);
             hashcode = hashcode % this.capacity;
         }
-
+        // console.log(`Key: ${key}, Hash: ${hashcode}`);
         return hashcode;
     }
 
     set(key, value) {
         let hashIndex = this.hash(key);
-        console.log(hashIndex);
+        // console.log(hashIndex);
         if (this.hashmap[this.hash(key)] != null) {
-            console.log(`Collision, adding node`);
+            // console.log(`Collision, adding node`);
             let currNode = this.hashmap[this.hash(key)].head; // Initialize to head
             while (currNode) {
                 if ((currNode.getValue()).includes(`${key}`)) {
@@ -32,9 +33,32 @@ class HashMap {
                 }
                 currNode = currNode.nextNode;
             }
-            console.log(`Adding node`);
+            // console.log(`Adding node`);
             this.size++;
             this.hashmap[this.hash(key)].append(`${key}, ${value}`);
+            // console.log(`Current size * load factor: ${this.loadFactor * this.size} `);
+            if ((this.size) >= (this.loadFactor * this.capacity)) {
+                console.log(`Expanding hashmap`);
+                const newHashmap = new HashMap(this.capacity * 2);
+                console.log(`Current capacity: ${this.capacity}`)
+                this.capacity = this.capacity * 2;
+                console.log(`New capacity: ${this.capacity}`)
+                this.hashmap.forEach((list) => {
+                    if (typeof list !== 'undefined') {
+                        let currNode = list.head
+                        while (currNode) {
+                            let currElement = currNode.value.split(', ');
+                            let currKey = currElement[0];
+                            let currValue = currElement[1];
+                            // console.log(currKey);
+                            // console.log(currValue);
+                            newHashmap.set(currKey, currValue);
+                            currNode = currNode.nextNode;
+                        };
+                    };
+                });
+                this.hashmap = newHashmap.hashmap;
+            }
             return;
         }
         else {
@@ -42,13 +66,36 @@ class HashMap {
             this.hashmap[this.hash(key)] = new LinkedList();
             this.size++;
             this.hashmap[this.hash(key)].append(`${key}, ${value}`);
+            // console.log(`Current size * load factor: ${this.loadFactor * this.size} `);
+            if ((this.size) >= (this.loadFactor * this.capacity)) {
+                console.log(`Expanding hashmap`);
+                const newHashmap = new HashMap(this.capacity * 2);
+                console.log(`Current capacity: ${this.capacity}`)
+                this.capacity = this.capacity * 2;
+                console.log(`New capacity: ${this.capacity}`)
+                this.hashmap.forEach((list) => {
+                    if (typeof list !== 'undefined') {
+                        let currNode = list.head
+                        while (currNode) {
+                            let currElement = currNode.value.split(', ');
+                            let currKey = currElement[0];
+                            let currValue = currElement[1];
+                            // console.log(currKey);
+                            // console.log(currValue);
+                            newHashmap.set(currKey, currValue);
+                            currNode = currNode.nextNode;
+                        };
+                    };
+                });
+                this.hashmap = newHashmap.hashmap;
+            }
             return;
         }
     }
 
     get(key) {
         let hashIndex = this.hash(key);
-        console.log(`Get: ${key}, Hash: ${hashIndex}`);
+        // console.log(`Get: ${key}, Hash: ${hashIndex}`);
         if (this.hashmap[hashIndex] != null) {
             let currNode = this.hashmap[hashIndex].head;
             while (currNode) {
@@ -63,7 +110,7 @@ class HashMap {
 
     has(key) {
         let hashIndex = this.hash(key);
-        console.log(`Has: ${key}, Hash: ${hashIndex}`);
+        // console.log(`Has: ${key}, Hash: ${hashIndex}`);
         if (this.hashmap[hashIndex] != null) {
             let currNode = this.hashmap[hashIndex].head;
             while (currNode) {
@@ -78,7 +125,7 @@ class HashMap {
 
     remove(key) {
         let hashIndex = this.hash(key);
-        console.log(`Remove: ${key}, Hash: ${hashIndex}`);
+        // console.log(`Remove: ${key}, Hash: ${hashIndex}`);
         if (this.hashmap[hashIndex] != null) {
             if (this.hashmap[hashIndex].size === 1) {
                 this.hashmap[hashIndex] = new LinkedList();
@@ -114,7 +161,7 @@ class HashMap {
                 return returnArray;
             }
             else {
-                console.log(`Null`);
+                // console.log(`Keys Null`);
             }
         }
     }
@@ -133,7 +180,7 @@ class HashMap {
                 return returnArray;
             }
             else {
-                console.log(`Null`);
+                // console.log(`Values Null`);
             }
         }
     }
@@ -142,19 +189,19 @@ class HashMap {
         let returnArray = new Array();
         for (let i = 0; i < this.capacity; i++) {
             if (this.hashmap[i] !== null && typeof this.hashmap[i] !== 'undefined') {
-                console.log(this.hashmap[i]);
+                // console.log(`Entries: ${this.hashmap[i]}`);
                 let currNode = this.hashmap[i].head;
                 while (currNode) {
-                    console.log(currNode.value);
+                    // console.log(`Entries: ${currNode.value}`);
                     returnArray.push(currNode.value);
                     currNode = currNode.nextNode;
                 }
-                return returnArray;
             }
             else {
-                console.log(`Null`);
+                // console.log(`Null entry, skipping.`);
             }
         }
+        return returnArray;
     }
 
     getLength() {
--- src/index.js
+++ src/index.js
@@ -4,49 +4,41 @@ import { HashMap } from './hashmap';
 console.log(`Welcome To The Odin Hashmap Project`);
 
 const newHash = new HashMap();
-const newHash2 = new HashMap(32);
 console.log(newHash.getLength());
-console.log(newHash2.getLength());
 
-console.log(`Apple Hash; ` + newHash.hash(`apple`));
-console.log(`New Hash; ` + newHash.hash(`b`));
-console.log(`Banana Hash; ` + newHash.hash(`banana`));
-console.log(`Berry Hash; ` + newHash.hash(`berry`));
-newHash.set('apple', 'red');
-newHash.hashmap[2].toString();
-
-newHash.set('b', 'blue');
-newHash.hashmap[2].toString();
-newHash.set('apple', 'fruit');
-newHash.hashmap[2].toString();
-
-console.log(newHash.get(`apple`));
-console.log(newHash.get(`b`));
-
-console.log(newHash.has(`apple`));
-console.log(newHash.has(`fl`));
-
-console.log(`Length: ${newHash.length()}`);
-
-newHash.hashmap[2].toString();
-console.log(newHash.remove(`apple`));
-
-console.log(`Length: ${newHash.length()}`);
-newHash.hashmap[2].toString();
-console.log(newHash.remove(`b`));
-
-console.log(`Length: ${newHash.length()}`);
-newHash.hashmap[2].toString();
-
-newHash.set('b', 'blue');
-newHash.hashmap[2].toString();
-newHash.set('apple', 'fruit');
-newHash.hashmap[2].toString();
-
-console.log(newHash.keys());
-console.log(newHash.values());
-console.log(newHash.entries());
-
-console.log(newHash.hashmap[2]);
 newHash.clear();
 console.log(newHash.hashmap[2]);
+console.log(newHash.entries());
+
+console.log(`Final Test`);
+newHash.set(`apple`, `red`);
+console.log(newHash.entries());
+newHash.set(`banana`, `yellow`);
+console.log(newHash.entries());
+newHash.set(`carrot`, `orange`);
+newHash.set(`dog`, `brown`);
+newHash.set(`elephant`, `gray`);
+newHash.set(`frog`, `green`);
+newHash.set(`grape`, `purple`);
+newHash.set(`hat`, `black`);
+newHash.set(`ice cream`, `white`);
+newHash.set(`jacket`, `blue`);
+newHash.set(`kite`, `pink`);
+console.log(newHash.entries());
+console.log(newHash.hashmap);
+newHash.set(`lion`, `golden`);
+newHash.set(`moon`, `silver`);
+console.log(newHash.entries());
+console.log(newHash.hashmap);
+console.log(newHash.capacity);
+console.log(newHash.entries());
+newHash.set(`nether`, `red`);
+newHash.set(`opal`, `rainbow`);
+console.log(newHash.entries());
+console.log(newHash.capacity);
+console.log(newHash.size);
+console.log(newHash.hashmap);
+console.log(newHash.has(`moon`));
+console.log(newHash.get(`moon`));
+
+// 15 entries
